7c094e5461de5ba1f3341daefc0d4ac0187a0635
purplecabbage <purplecabbage@gmail.com>
2012-08-08 18:17:57 -0700

    getting there ...

--- bin/cli.js
+++ bin/cli.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 
-var pluginstall = require('pluginstall'),
-    package = require('./package');
+var pluginstall = require('pluginstall');
+var pkg = require('package');
+var fs = require('fs');
 
 var args = process.argv.slice();
 var firstArgIndex = 0;
@@ -20,7 +21,7 @@ if (args.length == firstArgIndex) {
     console.log('Usage: cordova-ext-barcodescanner [platform] [project directory]');
 } 
 else if (args[firstArgIndex] === '-v') {
-    console.log('cordova-ext-echo version ' + package.version);
+    console.log('cordova-ext-echo version ' + pkg.version);
 } 
 else 
 {
@@ -30,8 +31,11 @@ else
     console.log("platform = " + platform);
     console.log("projectDir = " + projectDir);
 
-    console.log('cordova-ext-echo version ' + package.version);
-    console.log("Files = " + package.files);
+    console.log('cordova-ext-echo version ' + pkg.version);
+    console.log("Files = " + pkg.files);
+    
+    //var files = fs.readdirSync(pkg.files[0]);
+    //console.log("files = " + files);
 
     // console.log("platform = " + platform);
     // console.log("projectDir = " + projectDir);
