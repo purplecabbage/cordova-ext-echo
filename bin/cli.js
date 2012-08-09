9b2f2fbbd70b9bcc9cbdfeb130f1f86176ce2e75
purplecabbage <purplecabbage@gmail.com>
2012-08-09 12:55:01 -0700

    resolves itself and calls pluginstall

--- bin/cli.js
+++ bin/cli.js
@@ -1,54 +1,55 @@
 #!/usr/bin/env node
 
-var pluginstall = require('pluginstall');
-var pkg = require('package');
-var fs = require('fs');
+(function(){
+    
+    
+    var pluginstall = require('pluginstall');
+    var pkg = require('../package');
+    var fs = require('fs');
+    var path = require('path');
+    var args = process.argv.slice();
+    var firstArgIndex = 0;
 
-var args = process.argv.slice();
-var firstArgIndex = 0;
+    // note: windows expands node to the full node.exe path
+    if (args[0] == "node" || args[0].indexOf("node.exe") > 0) {
+        firstArgIndex++;
+    }
 
-// note: windows expands node to the full node.exe path
-if (args[0] == "node" || args[0].indexOf("node.exe") > 0) {
+    //args.shift() // skip "cli.js"
     firstArgIndex++;
-}
 
-//args.shift() // skip "cli.js"
-firstArgIndex++;
+    //console.log("args.length = " + args.length + " : " + firstArgIndex + " " + args);
 
-//console.log("args.length = " + args.length + " : " + firstArgIndex + " " + args);
-
-if (args.length == firstArgIndex) {
-    console.log('Usage: cordova-ext-barcodescanner [platform] [project directory]');
-} 
-else if (args[firstArgIndex] === '-v') {
-    console.log('cordova-ext-echo version ' + pkg.version);
-} 
-else 
-{
-    platform = args[firstArgIndex];
-    projectDir = args[firstArgIndex+1];
+    if (args.length == firstArgIndex) {
+        console.log('Usage: cordova-ext-echo [platform] [project directory]');
+    } 
+    else if (args[firstArgIndex] === '-v') {
+        console.log('cordova-ext-echo version ' + pkg.version);
+    } 
+    else {
     
-    console.log("platform = " + platform);
-    console.log("projectDir = " + projectDir);
+        var platform = args[firstArgIndex];
+        var projectDir = args[firstArgIndex+1];
+    
+    //    console.log("platform = " + platform);
+    //    console.log("projectDir = " + projectDir);
+    //    console.log('cordova-ext-echo version ' + pkg.version);
+    
+        var pkgPath = require.resolve(pkg.name); // find yourself little one!
+        var extSrcDir = path.join(path.dirname(pkgPath), "src");
+    
+    //    console.log("extSrcDir = " + extSrcDir);
+    
+        var config = pluginstall.init(platform, projectDir, extSrcDir);
+        var plugin = pluginstall.parseXml(config);
 
-    console.log('cordova-ext-echo version ' + pkg.version);
-    console.log("Files = " + pkg.files);
-    
-    //var files = fs.readdirSync(pkg.files[0]);
-    //console.log("files = " + files);
+        pluginstall.installPlugin(config, plugin, function (err) {
+            if (err) {
+                console.error(err);
+            } else {
+                console.log('plugin installed for platform::' + platform);
+            }
+        });    
+    }
 
-    // console.log("platform = " + platform);
-    // console.log("projectDir = " + projectDir);
-    // config = pluginstall.init(platform, projectDir);
-    // plugin = pluginstall.parseXml(config);
-    // 
-    // pluginstall.installPlugin(config, plugin, function (err) {
-    //     if (err) {
-    //         console.error(err);
-    //     } else {
-    //         console.log('plugin installed for platform::' + platform);
-    //     }
-    // });
-    
-    
-}
+})();
