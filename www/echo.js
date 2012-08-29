d16ecc09e24fccfdbbf483d13e7f489e5c37502b
Fil Maj <maj.fil@gmail.com>
2012-08-29 14:15:26 -0700

    renaming echo plugin service label

--- www/echo.js
+++ www/echo.js
@@ -4,7 +4,7 @@
     window.echo = function(str, callback) {
         cordova.exec(callback, function(err) {
             callback('Nothing to echo.');
-        }, "Echo", "echo", [str]);
+        }, "EchoPlugin", "echo", [str]);
     };    
     
 })(window);
