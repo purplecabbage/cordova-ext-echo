decc28aeb1ae5bf57801d951fe43a7cf29a9aae0
srikanthnadella <srikanth.nadella@gmail.com>
2014-09-30 20:32:45 -0400

    Update echo.js

--- www/echo.js
+++ www/echo.js
@@ -3,7 +3,7 @@
     window.echo = function(str, callback) {
         cordova.exec(callback, function(err) {
             callback('Nothing to echo.');
-        }, "org.apache.cordova.plugins.Echo", "echo", [str]);
+        }, "Echo", "echo", [str]);
     };    
     
 })(window);
