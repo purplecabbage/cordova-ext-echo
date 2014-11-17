85fc2c3d7e756500d2048aa7bc2041de66a91030
ybian <bianying@gmail.com>
2014-11-17 17:37:52 +0800

    changes to work with cordova 4.0.0

--- www/echo.js
+++ www/echo.js
@@ -1,10 +1,13 @@
-(function(window){
-    
-    window.echo = function(str, callback) {
-        cordova.exec(callback, function(err) {
-            callback('Nothing to echo.');
-        }, "Echo", "echo", [str]);
-    };    
-    
-})(window);
+var exec = cordova.require('cordova/exec');
 
+var Echo = function(){
+
+};
+
+Echo.echo = function(str, callback) {
+  exec(callback, function(err) {
+      callback('Nothing to echo.');
+  }, "Echo", "echo", [str]);
+};
+
+module.exports = Echo;
